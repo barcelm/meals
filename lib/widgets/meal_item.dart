2c2f1e237bbadb007cd356b80c76e6974c9f8fa4
Bartłomiej Sobczyk <barcelm10@gmail.com>
2023-12-28 18:25:29 +0100

    Add files via upload

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
 
   final Meal meal;
+
+  String get complexityText{
+    return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1);
+  }
+  String get affordAbility{
+    return meal.affordability.name[0].toUpperCase() + meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,8 +59,17 @@ class MealItem extends StatelessWidget {
                     const SizedBox(
                       height: 16,
                     ),
-                    Row(
-                      children: [],
+                    Row(mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                            icon: Icons.schedule, label: '${meal.duration} min'),//meal.duration.toString())
+                            const SizedBox(width: 16,),
+                            MealItemTrait(
+                            icon: Icons.work, label: complexityText),
+                            const SizedBox(width: 16,),
+                            MealItemTrait(
+                            icon: Icons.attach_money, label: affordAbility)
+                      ], 
                     )
                   ],
                 ),
